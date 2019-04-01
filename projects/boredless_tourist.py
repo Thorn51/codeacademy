12a99c0e3f5f06ce1d2cc91324552c82ade3a4e4
Matt Thornberry <mtberry16@hotmail.com>
2019-04-01 17:36:51 -0600

    Created attractions and functionality for adding new attractions

--- projects/boredless_tourist.py
+++ projects/boredless_tourist.py
@@ -1,2 +1,41 @@
-destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+
+def get_destination_index(destination):
+    try:
+        destination_index = destinations.index(destination)
+        return destination_index
+    except ValueError:
+        return "No Informatoin on Destination"
+
+
+def get_traveler_location(traveler):
+    traveler_destination = test_traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+attractions = [[] for attraction in destinations]
+
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index].append(attraction)
+    except ValueError:
+        return "No information on location!"
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(get_destination_index("Denver"))
