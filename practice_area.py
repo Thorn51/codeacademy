a867d6a9f82922bd1ced4497512cd526fa89e69b
Matt Thornberry <mtberry16@hotmail.com>
2019-04-03 08:23:40 -0600

    I'm trying a second test sending from PyCharm to GitHub to see how deleting a block of code and replacing it works.

--- practice_area.py
+++ practice_area.py
@@ -1,11 +1,8 @@
-first_name = "Reiko"
-last_name = "Matsuki"
+def common_letters(string_one, string_two):
+    common = []
+    for letter in string_one:
+        if (letter in string_two) and not (letter in common):
+            common.append(letter)
+    return common
 
-
-def password_generator(first_name, last_name):
-    temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
-    return temp_password
-
-
-temp_password = password_generator(first_name, last_name)
-print(temp_password)
+print(common_letters("banana", "cream"))
