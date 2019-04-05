e1afd7bd014ddb67e1f13c2cf4ef0e08a2c91902
Matt Thornberry <mtberry16@hotmail.com>
2019-04-04 20:40:04 -0600

    I'm trying a second test sending from PyCharm to GitHub to see how deleting a block of code and replacing it works.

--- practice_area.py
+++ practice_area.py
@@ -1,8 +1,6 @@
-def common_letters(string_one, string_two):
-    common = []
-    for letter in string_one:
-        if (letter in string_two) and not (letter in common):
-            common.append(letter)
-    return common
 
-print(common_letters("banana", "cream"))
+user_name = "::::::::Eloise :::::::::::"
+
+user_name_fixed = user_name.strip(":").strip()
+
+print(user_name_fixed)
