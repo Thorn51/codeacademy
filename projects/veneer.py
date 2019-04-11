2067fe8f1a20071ee2b1c091cd4a3c3a6da769af
Matt Thornberry <mtberry16@hotmail.com>
2019-04-11 17:52:30 -0600

    Added class MarketPlace with methods for adding listings, removing listings, and showing listings

--- projects/veneer.py
+++ projects/veneer.py
@@ -9,6 +9,23 @@ class Art:
     def __repr__(self):
         return self.representative_string
 
+class MarketPlace:
+    def __init__(self):
+        self.listings = []
+
+    def add_listings(self, new_listings):
+        self.listings.append(new_listings)
+
+    def remove_listings(self, sold):
+        self.listings.remove(sold)
+
+    def show_listings(self):
+        for listing in self.listings:
+            print(self.listings)
+
+
+veneer = MarketPlace()
+
 girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", "oil on canvas", 1910)
 
-print(girl_with_mandolin)
+print(veneer.show_listings())
