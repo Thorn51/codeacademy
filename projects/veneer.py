6aa3012a78da9d2838e4a6fa1a4dd8f829c34229
Matt Thornberry <mtberry16@hotmail.com>
2019-04-15 10:46:56 -0600

    Finished the veneer project, not as strong at the end as I would like. Need to review this project.

--- projects/veneer.py
+++ projects/veneer.py
@@ -1,31 +1,77 @@
-class Art:
-    def __init__(self, artist, title, medium, year):
-        self.artist = artist
-        self.title = title
-        self.medium = medium
-        self.year = year
-        self.representative_string = """{artist}. "{title}". {year}, {medium}""".format(artist=self.artist, title=self.title, year=self.year, medium=self.medium)
-
-    def __repr__(self):
-        return self.representative_string
-
 class MarketPlace:
     def __init__(self):
         self.listings = []
 
-    def add_listings(self, new_listings):
-        self.listings.append(new_listings)
+    def add_listing(self, new_listing):
+        self.listings.append(new_listing)
 
-    def remove_listings(self, sold):
-        self.listings.remove(sold)
+    def remove_listings(self, expired_listing):
+        self.listings.remove(expired_listing)
 
     def show_listings(self):
         for listing in self.listings:
-            print(self.listings)
+            print(listing)
+
+class Art:
+    def __init__(self, artist, title, medium, year, owner):
+        self.artist = artist
+        self.title = title
+        self.medium = medium
+        self.year = year
+        self.owner = owner
+
+    def __repr__(self):
+        return f"""{self.artist}. "{self.title}." {self.year}, {self.medium}. {self.owner.name}, {self.owner.location}."""
+
+class Client:
+    def __init__(self, name, location, is_museum):
+        self.name = name
+        self.is_museum = is_museum
+        if is_museum:
+            self.location = location
+        else:
+            self.location = "Private Collection"
+
+    def sell_artwork(self, artwork, price):
+        if artwork.owner == self:
+            new_listing = Listing(artwork, price, self)
+            veneer.add_listing(new_listing)
+
+    def buy_artwork(self, artowork):
+        if artowork.owner != self:
+            art_listing = None
+            for listing in veneer.listings:
+                if listing.art == artowork:
+                    art_listing = listing
+                    break
+            if art_listing != None:
+                art_listing.art.owner = self
+                veneer.remove_listings(art_listing)
+
+class Listing:
+    def __init__(self, art, price, seller):
+        self.art = art
+        self.price = price
+        Client.name = seller
+
+    def __repr__(self):
+        return f"""{self.art.title} -- {self.price}"""
 
 
 veneer = MarketPlace()
 
-girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", "oil on canvas", 1910)
+edytta = Client("Edytta Halpirt", None, False)
 
-print(veneer.show_listings())
+moma = Client("The MOMA", "New York", True)
+
+girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", "oil on canvas", 1910, edytta)
+
+edytta.sell_artwork(girl_with_mandolin, "$6M (USD)")
+
+veneer.show_listings()
+
+moma.buy_artwork(girl_with_mandolin)
+
+print(girl_with_mandolin)
+
+veneer.show_listings()
